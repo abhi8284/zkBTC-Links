e657dfa7e9e9c645b4ba30159fcf8c32d2b77135
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2024-08-27 07:42:25 +0000

    create BTC-Links

--- BTC-Links-frontend/app/page.tsx
+++ BTC-Links-frontend/app/page.tsx
@@ -1,4 +1,3 @@
-import { Header } from "../sections/Header";
 import { Hero } from "../sections/Hero";
 import { LogoTicker } from "../sections/LogoTicker";
 import { ProductShowcase } from "../sections/ProductShowcase";
@@ -10,7 +9,6 @@ import { Footer } from "../sections/Footer";
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
       <LogoTicker />
       <ProductShowcase />
--- BTC-Links-frontend/sections/Header.tsx
+++ BTC-Links-frontend/sections/Header.tsx
@@ -2,11 +2,12 @@ import ArrowRight from "@/assets/arrow-right.svg";
 import Logo from "@/assets/logosaas.png";
 import Image from "next/image";
 import MenuIcon from "@/assets/menu.svg";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="sticky top-0 backdrop-blur-sm z-20">
-      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+      {/* <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">
           Streamline your workflow and boost your productivity
         </p>
@@ -14,7 +15,7 @@ export const Header = () => {
           <p>Get started for free</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
         </div>
-      </div>
+      </div> */}
       <div className="py-5">
         <div className="container">
           <div className="flex items-center justify-between">
@@ -23,10 +24,11 @@ export const Header = () => {
             <nav className="hidden md:flex gap-6 text-black/60 items-center">
               <a href="#">About</a>
               <a href="#">Features</a>
-              <a href="#">Customers</a>
-              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
-                Get Started
-              </button>
+              <Link href="/create" >
+                <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
+                  Get Started
+                </button>
+              </Link>
             </nav>
           </div>
         </div>
