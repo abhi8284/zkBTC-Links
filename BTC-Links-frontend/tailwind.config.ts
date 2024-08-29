0750fc673ec5913fef4723978d4e2f10d7d65365
Kamal Nayan <geniusamansingh@gmail.com>
2024-08-29 01:22:02 +0000

    fix the tailwindcss bug

--- BTC-Links-frontend/tailwind.config.ts
+++ BTC-Links-frontend/tailwind.config.ts
@@ -3,21 +3,25 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./sections/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
     },
     extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "20px",
+          lg: "80px",
+        }},
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
